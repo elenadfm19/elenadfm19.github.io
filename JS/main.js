415e887377cb9ab9c836e1af2ac0789b967614a2
elenadfm19 <elenadefrias@gmail.com>
2025-05-04 13:47:31 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -6,50 +6,89 @@ let p3 = document.getElementById('project3');
 let p4 = document.getElementById('project4');
 let projectPhoto = document.getElementById('project-photo');
 
-p1.addEventListener('mouseover', function(){
-    projectPhoto.style.display = 'block';
-    projectPhoto.src = './resources/images/project1_photo.png';
-});
-p1.addEventListener('mouseout', function(){
-    projectPhoto.style.display = 'none';
-});
+function isTouchDevice() {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+}
 
-p2.addEventListener('mouseover', function(){
-    projectPhoto.style.display = 'block';
-    projectPhoto.src = './resources/images/project2_photo.png';
-});
-p2.addEventListener('mouseout', function(){
-    projectPhoto.style.display = 'none';
-});
+if (isTouchDevice()) {
+    p1.addEventListener('touchstart', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project1_photo.png';
+    });
+    p1.addEventListener('touchend', function () {
+        projectPhoto.style.display = 'none';
+    });
 
-p3.addEventListener('mouseover', function(){
-    projectPhoto.style.display = 'block';
-    projectPhoto.src = './resources/images/project3_photo.png';
-});
-p3.addEventListener('mouseout', function(){
-    projectPhoto.style.display = 'none';
-});
+    p2.addEventListener('touchstart', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project2_photo.png';
+    });
+    p2.addEventListener('touchend', function () {
+        projectPhoto.style.display = 'none';
+    });
 
-p4.addEventListener('mouseover', function(){
-    projectPhoto.style.display = 'block';
-    projectPhoto.src = './resources/images/project4_photo.png';
-});
-p4.addEventListener('mouseout', function(){
-    projectPhoto.style.display = 'none';
-});
+    p3.addEventListener('touchstart', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project3_photo.png';
+    });
+    p3.addEventListener('touchend', function () {
+        projectPhoto.style.display = 'none';
+    });
+
+    p4.addEventListener('touchstart', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project4_photo.png';
+    });
+    p4.addEventListener('touchend', function () {
+        projectPhoto.style.display = 'none';
+    });
+} else {
+    p1.addEventListener('mouseover', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project1_photo.png';
+    });
+    p1.addEventListener('mouseout', function () {
+        projectPhoto.style.display = 'none';
+    });
+
+    p2.addEventListener('mouseover', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project2_photo.png';
+    });
+    p2.addEventListener('mouseout', function () {
+        projectPhoto.style.display = 'none';
+    });
+
+    p3.addEventListener('mouseover', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project3_photo.png';
+    });
+    p3.addEventListener('mouseout', function () {
+        projectPhoto.style.display = 'none';
+    });
+
+    p4.addEventListener('mouseover', function () {
+        projectPhoto.style.display = 'block';
+        projectPhoto.src = './images/project4_photo.png';
+    });
+    p4.addEventListener('mouseout', function () {
+        projectPhoto.style.display = 'none';
+    });
+}
+
 
 
 /*We show the drop-down menu when we click on the menu logo for small screen sizes*/
 let menuIcon = document.getElementById('menu');
 let dropDownMenu = document.getElementById('drop-down');
-let clicked=0;
+let clicked = 0;
 
-menuIcon.addEventListener('click',function(){
-    if (clicked===0){
-        dropDownMenu.style.display='block';
-        clicked=1;
-    }else{
-        dropDownMenu.style.display='none';
-        clicked=0;
+menuIcon.addEventListener('click', function () {
+    if (clicked === 0) {
+        dropDownMenu.style.display = 'block';
+        clicked = 1;
+    } else {
+        dropDownMenu.style.display = 'none';
+        clicked = 0;
     }
 });
